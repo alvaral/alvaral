e7078ed9375571877fdcfb10adb851d04602ea88
Álvaro <alvaroalonso222@gmail.com>
2025-06-29 23:26:25 +0200

    fix cookie language request

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,13 +1,18 @@
 import { getRequestConfig } from "next-intl/server";
 import { cookies } from "next/headers";
 
+const SUPPORTED_LOCALES = ["en", "es"];
+const DEFAULT_LOCALE = "en";
+
 export default getRequestConfig(async () => {
-    const cookieStore = cookies();
-    const cookieLocale = (await cookieStore).get("ALVARAL_LOCALE")?.value;
-  
-    const locale = cookieLocale || "en";
-    return {
-        locale,
-        messages: (await import(`../messages/${locale}.json`)).default
-    };
+  const cookieStore =  await cookies();
+  const cookieLocale = cookieStore.get("ALVARAL_LOCALE")?.value;
+
+  const locale: string =
+    SUPPORTED_LOCALES.includes(cookieLocale || "") ? cookieLocale! : DEFAULT_LOCALE;
+
+  return {
+    locale,
+    messages: (await import(`../messages/${locale}.json`)).default
+  };
 });
