6cfa514e9a3adc85f27da07b31e0719f47188d3a
Álvaro <alvaroalonso222@gmail.com>
2025-07-06 15:40:02 +0200

    add analytics: umami

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
+// app/layout.tsx
 import type { Metadata } from "next";
 import  { NextIntlClientProvider } from "next-intl";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { getLocale, getMessages } from "next-intl/server";
+import Script from 'next/script';
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +32,14 @@ export default async function RootLayout({
   const locale = await getLocale();
   return (
     <html lang={locale}>
+      <head>
+        <Script
+          async
+          defer
+          data-website-id="748703d0-709c-4105-adfd-66625c8d2139"
+          src="https://umami-alvaral.onrender.com/script.js"
+        />
+      </head>
       <body 
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
