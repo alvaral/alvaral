2a866f4c2628380f3132174fb979c27bdd1d8360
Álvaro Alonso <alvaro.alonso@xeridia.com>
2025-10-25 01:47:10 +0200

    fix 2

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -3,6 +3,10 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+export type TextareaProps = React.DetailedHTMLProps<
+  React.TextareaHTMLAttributes<HTMLTextAreaElement>,
+  HTMLTextAreaElement
+>;
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
